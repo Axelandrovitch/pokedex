6a6cc6e83652be4ee89fc961f9c6fdf86a9e9a48
Axelandrovitch <axel.a.hetru@gmail.com>
2025-03-19 14:45:19 +0100

    add commands support logic. Add exit command + help command

--- main.go
+++ main.go
@@ -7,8 +7,31 @@ import (
 	"strings"
 )
 
-func main() {
+type cliCommand struct {
+	Name        string
+	Description string
+	Callback    func() error
+}
 
+var supportedCommands map[string]cliCommand
+
+func initCommands() {
+	supportedCommands = map[string]cliCommand{
+		"exit": {
+			Name:        "exit",
+			Description: "Exit the Pokedex",
+			Callback:    commandExit,
+		},
+		"help": {
+			Name:        "help",
+			Description: "Lists available commands",
+			Callback:    commandHelp,
+		},
+	}
+}
+
+func main() {
+	initCommands()
 	fmt.Println(readFromStdin())
 }
 
@@ -24,10 +47,37 @@ func cleanInput(text string) []string {
 
 func readFromStdin() []string {
 	for {
-		fmt.Print("Pokedex >")
+		fmt.Print("Pokedex > ")
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
 		input := cleanInput(scanner.Text())
-		fmt.Printf("Your command was: %s\n", input[0])
+		if len(input[0]) > 0 {
+			command := input[0]
+			executeUserCommand(command)
+		}
 	}
+}
+
+func executeUserCommand(command string) error {
+	if cmd, validCommand := supportedCommands[command]; validCommand {
+		return cmd.Callback()
+	}
+	fmt.Println("Unknown command")
+	return fmt.Errorf("Unknown command")
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return fmt.Errorf("Error exiting the program")
+}
+
+func commandHelp() error {
+	fmt.Println("Usage <.pokedex> <command>")
+	fmt.Println("Available commands:")
+	for _, cmd := range supportedCommands {
+		fmt.Println(cmd.Name)
+		fmt.Printf("\t %s\n", cmd.Description)
+	}
+	return fmt.Errorf("Error executing help command")
 }
