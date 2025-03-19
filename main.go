6893e390c57386e46be2e5e3f78af4d818b281eb
Axelandrovitch <axel.a.hetru@gmail.com>
2025-03-19 13:18:57 +0100

    add basic logic to read from stdin + ignore .log files

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	fmt.Println(readFromStdin())
 }
 
 func cleanInput(text string) []string {
@@ -17,4 +20,14 @@ func cleanInput(text string) []string {
 	split := strings.Fields(trimmed)
 
 	return split
+}
+
+func readFromStdin() []string {
+	for {
+		fmt.Print("Pokedex >")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+		fmt.Printf("Your command was: %s\n", input[0])
+	}
 }
